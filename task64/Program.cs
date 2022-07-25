58e62d88b16cf23340f3c1a9be07176fb9c9018a
DmitryZ <zagarskih@inbox.ru>
2022-07-25 23:01:50 +0500

    тест аккермана

---
+++ task64/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+void SumDigits(int m, int n, int summ = 0)
+{
+    if (m > n)
+    {
+        System.Console.WriteLine(summ);
+        return;
+    }
+    summ = summ + m;
+    SumDigits(m + 1, n, summ);
+}
+
+System.Console.WriteLine("введите число M");
+int m = Int32.Parse(Console.ReadLine());
+System.Console.WriteLine("введите число N");
+int n = Int32.Parse(Console.ReadLine());
+SumDigits(m, n);
---
+++ task66/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 66: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+
+System.Console.WriteLine("введите число M");
+int m = Int32.Parse(Console.ReadLine());
+System.Console.WriteLine("введите число N");
+int n = Int32.Parse(Console.ReadLine());
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+        System.Console.WriteLine(n + 1);
+    }
+    else
+    if (m > 0 && n == 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    if (m > 0 && n > 0)
+    {
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+    }
+}
